ef35673ccf6ea13c36cb923098f55c55066da67c
Diego Ugarte <diego.ugarte@outlook.com>
2021-08-21 16:58:29 +0900

    :sparkles: Add option for setting default values

--- src/argparse.zig
+++ src/argparse.zig
@@ -44,6 +44,7 @@ pub const ArgumentParserOption = struct {
     argument_type: type = bool,
     takes: enum { None, One, Many } = .None,
     required: bool = false,
+    default_value: ?union { int: comptime_int, float: comptime_float, string: []const u8, boolean: bool } = null,
 };
 
 pub fn ArgumentParser(comptime config: ParserConfig, comptime options: anytype) type {
@@ -138,16 +139,29 @@ pub fn ArgumentParser(comptime config: ParserConfig, comptime options: anytype) 
             try displayOptionsWriter(stdout);
         }
 
-        pub const ParserResult = blk: {
+        pub const ParserResult = blk_outer: {
             // Struct fields
             var fields: [options.len]StructField = undefined;
             inline for (options) |option, i| {
                 fields[i] = .{
                     .name = option.name,
                     .field_type = switch (option.takes) {
-                        .None => bool,
-                        .One => ?option.argument_type,
-                        .Many => ArrayList(option.argument_type),
+                        .None => blk_inner: {
+                            if (option.argument_type != bool) @compileError("Option that takes no argument (flag) can only be of type bool");
+                            break :blk_inner bool;
+                        },
+                        .One => blk_inner: {
+                            if (option.argument_type == bool) @compileError("Option that takes arguments (non flag) can't be of type bool");
+                            if (option.required) {
+                                break :blk_inner option.argument_type;
+                            } else {
+                                break :blk_inner ?option.argument_type;
+                            }
+                        },
+                        .Many => blk_inner: {
+                            if (option.argument_type == bool) @compileError("Option that takes arguments (non flag) can't be of type bool");
+                            break :blk_inner ArrayList(option.argument_type);
+                        },
                     },
                     .default_value = null,
                     .is_comptime = false,
@@ -158,7 +172,7 @@ pub fn ArgumentParser(comptime config: ParserConfig, comptime options: anytype) 
             // Struct declarations
             var decls: [0]Declaration = .{};
 
-            break :blk @Type(TypeInfo{ .Struct = .{
+            break :blk_outer @Type(TypeInfo{ .Struct = .{
                 .layout = .Auto,
                 .fields = &fields,
                 .decls = &decls,
@@ -259,7 +273,7 @@ pub fn ArgumentParser(comptime config: ParserConfig, comptime options: anytype) 
                                 }
                                 const next_arg = arguments[i + 1][0..len(arguments[i + 1])];
                                 switch (@typeInfo(option.argument_type)) {
-                                    .Int => @field(parsed_args, option.name) = try fmt.parseInt(option.argument_type, next_arg),
+                                    .Int => @field(parsed_args, option.name) = try fmt.parseInt(option.argument_type, next_arg, 10),
                                     .Float => @field(parsed_args, option.name) = try fmt.parseFloat(option.argument_type, next_arg),
                                     .Pointer => @field(parsed_args, option.name) = next_arg,
                                     else => unreachable,
@@ -288,7 +302,7 @@ pub fn ArgumentParser(comptime config: ParserConfig, comptime options: anytype) 
                                         if (eql(u8, next_arg, opt.short orelse "") or eql(u8, next_arg, opt.long orelse "")) break :search_loop;
                                     }
                                     switch (@typeInfo(option.argument_type)) {
-                                        .Int => try @field(parsed_args, option.name).append(try fmt.parseInt(option.argument_type, next_arg)),
+                                        .Int => try @field(parsed_args, option.name).append(try fmt.parseInt(option.argument_type, next_arg, 10)),
                                         .Float => try @field(parsed_args, option.name).append(try fmt.parseFloat(option.argument_type, next_arg)),
                                         .Pointer => try @field(parsed_args, option.name).append(next_arg),
                                         else => unreachable,
@@ -302,23 +316,6 @@ pub fn ArgumentParser(comptime config: ParserConfig, comptime options: anytype) 
                     }
                 }
 
-                inline for (options) |option, id| {
-                    if (!parsing_done[id] and option.required) {
-                        if (comptime config.display_error) {
-                            const long = option.long orelse "";
-                            const short = option.short orelse "";
-                            const separator = if (option.short != null) (if (option.long != null) ", " else "") else "";
-
-                            const long_fmt = bold ++ green ++ long ++ reset;
-                            const short_fmt = bold ++ green ++ short ++ reset;
-                            const error_fmt = bold ++ red ++ "Error:" ++ reset;
-                            try writer.writeAll(error_fmt ++ " Missing required argument " ++ short_fmt ++ separator ++ long_fmt ++ "\n");
-                        }
-
-                        return error.MissingArgument;
-                    }
-                }
-
                 if (comptime config.display_error) {
                     const arg_fmt = bold ++ green ++ "{s}" ++ reset;
                     const error_fmt = bold ++ red ++ "Error:" ++ reset;
@@ -326,6 +323,50 @@ pub fn ArgumentParser(comptime config: ParserConfig, comptime options: anytype) 
                 }
 
                 return error.UnknownArgument;
+            }
+
+            // Check for required arguments
+            inline for (options) |option, id| {
+                if (!parsing_done[id] and option.required) {
+                    if (comptime config.display_error) {
+                        const long = option.long orelse "";
+                        const short = option.short orelse "";
+                        const separator = if (option.short != null) (if (option.long != null) ", " else "") else "";
+
+                        const long_fmt = bold ++ green ++ long ++ reset;
+                        const short_fmt = bold ++ green ++ short ++ reset;
+                        const error_fmt = bold ++ red ++ "Error:" ++ reset;
+                        try writer.writeAll(error_fmt ++ " Missing required argument " ++ short_fmt ++ separator ++ long_fmt ++ "\n");
+                    }
+
+                    return error.MissingArgument;
+                }
+            }
+
+            // Add default values
+            inline for (options) |option, id| {
+                if (!parsing_done[id]) {
+                    if (option.default_value) |default| {
+                        switch (option.takes) {
+                            .None => @field(parsed_args, option.name) = default.boolean,
+                            .One => {
+                                if (option.required) @compileError("Required option can't have a default value.");
+                                switch (@typeInfo(option.argument_type)) {
+                                    .Int => @field(parsed_args, option.name) = default.int,
+                                    .Float => @field(parsed_args, option.name) = default.float,
+                                    .Pointer => @field(parsed_args, option.name) = default.string,
+                                    else => unreachable,
+                                }
+                            },
+                            .Many => switch (@typeInfo(option.argument_type)) {
+                                .Int => try @field(parsed_args, option.name).append(try fmt.parseInt(option.argument_type, default.int, 10)),
+                                .Float => try @field(parsed_args, option.name).append(try fmt.parseFloat(option.argument_type, default.float)),
+                                .Pointer => try @field(parsed_args, option.name).append(default.string),
+                                else => unreachable,
+                            },
+                        }
+                    }
+                }
             }
 
             return parsed_args;
@@ -580,6 +621,37 @@ test "Argparse parseArgumentsWriter 3" {
             .name = "foo",
             .short = "-f",
             .description = "",
+            .default_value = .{ .boolean = true },
+        },
+    });
+
+    const arguments = [_][*:0]const u8{"test"};
+
+    const parsed_args = try Parser.parseArgumentsWriter(testing.allocator, &arguments, w);
+    defer Parser.deinitArgs(parsed_args);
+
+    try testing.expectEqual(bool, @TypeOf(parsed_args.foo));
+    try testing.expectEqual(true, parsed_args.foo);
+}
+
+test "Argparse parseArgumentsWriter 4" {
+    // Initialize array list
+    var list = std.ArrayList(u8).init(testing.allocator);
+    defer list.deinit();
+
+    // Get writer
+    const w = list.writer();
+
+    const Parser = ArgumentParser(.{
+        .bin_name = "",
+        .bin_info = "",
+        .bin_usage = "",
+        .bin_version = .{ .major = 1, .minor = 2, .patch = 3 },
+    }, [_]ArgumentParserOption{
+        .{
+            .name = "foo",
+            .short = "-f",
+            .description = "",
             .required = true,
         },
     });
@@ -587,4 +659,70 @@ test "Argparse parseArgumentsWriter 3" {
     const arguments = [_][*:0]const u8{"test"};
 
     try testing.expectError(Parser.ParserError.NoArgument, Parser.parseArgumentsWriter(testing.allocator, &arguments, w));
+}
+
+test "Argparse parseArgumentsWriter 5" {
+    // Initialize array list
+    var list = std.ArrayList(u8).init(testing.allocator);
+    defer list.deinit();
+
+    // Get writer
+    const w = list.writer();
+
+    const Parser = ArgumentParser(.{
+        .bin_name = "",
+        .bin_info = "",
+        .bin_usage = "",
+        .bin_version = .{ .major = 1, .minor = 2, .patch = 3 },
+    }, [_]ArgumentParserOption{
+        .{
+            .name = "foo",
+            .short = "-f",
+            .description = "",
+            .takes = .One,
+            .argument_type = i32,
+            .default_value = .{ .int = -10 },
+        },
+    });
+
+    const arguments = [_][*:0]const u8{"test"};
+
+    const parsed_args = try Parser.parseArgumentsWriter(testing.allocator, &arguments, w);
+    defer Parser.deinitArgs(parsed_args);
+
+    try testing.expectEqual(?i32, @TypeOf(parsed_args.foo));
+    try testing.expect(parsed_args.foo.? == -10);
+}
+
+test "Argparse parseArgumentsWriter 6" {
+    // Initialize array list
+    var list = std.ArrayList(u8).init(testing.allocator);
+    defer list.deinit();
+
+    // Get writer
+    const w = list.writer();
+
+    const Parser = ArgumentParser(.{
+        .bin_name = "",
+        .bin_info = "",
+        .bin_usage = "",
+        .bin_version = .{ .major = 1, .minor = 2, .patch = 3 },
+    }, [_]ArgumentParserOption{
+        .{
+            .name = "foo",
+            .short = "-f",
+            .description = "",
+            .takes = .One,
+            .argument_type = f64,
+            .default_value = .{ .float = -10 },
+        },
+    });
+
+    const arguments = [_][*:0]const u8{"test"};
+
+    const parsed_args = try Parser.parseArgumentsWriter(testing.allocator, &arguments, w);
+    defer Parser.deinitArgs(parsed_args);
+
+    try testing.expectEqual(?f64, @TypeOf(parsed_args.foo));
+    try testing.expect(parsed_args.foo.? == -10);
 }
